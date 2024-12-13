4da8abbd7411c14ac4725902543f88d04e047d29
Defri <defrilugas46@gmail.com>
2024-12-14 00:12:24 +0700

    initial commit

--- app/src/main/java/com/deeyat/d_garage/spareparts.java
+++ app/src/main/java/com/deeyat/d_garage/spareparts.java
@@ -3,13 +3,17 @@ package com.deeyat.d_garage;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class spareparts extends AppCompatActivity {
 
@@ -25,41 +29,87 @@ public class spareparts extends AppCompatActivity {
             startActivity(intent);
         });
 
+        TextView textView68 = findViewById(R.id.textView68);
+        textView68.setText(Html.fromHtml("<u>Oli Transmisi</u>"));
+
+        // Event listener untuk membuka URL pada setiap ImageView
+        initializeImageView(R.id.imageView75, "https://www.top1.co.id/produk/detail/EVOLUTION-5W-30-API-SP");
+        initializeImageView(R.id.imageView2, "https://www.top1.co.id/produk/detail/EVOLUTION-0W-20-API-SP");
+        initializeImageView(R.id.imageView3, "https://www.top1.co.id/produk/detail/EVOLUTION-5W-40-API-SP");
+        initializeImageView(R.id.imageView4, "https://www.top1.co.id/produk/detail/EVOLUTION-5W-40-API-SP");
+        initializeImageView(R.id.imageView6, "https://www.top1.co.id/produk/detail/EVOLUTION-DIESEL-5W-40-API-CJ-4");
+        initializeImageView(R.id.imageView12, "https://www.top1.co.id/produk/detail/ZENZATION-5W-30-API-SP");
+        initializeImageView(R.id.imageView14, "https://www.top1.co.id/produk/detail/ZENZATION-5W-40-API-SP");
+        initializeImageView(R.id.imageView16, "https://www.top1.co.id/produk/detail/ZENZATION-0W-20-API-SN");
+        initializeImageView(R.id.imageView18, "https://www.top1.co.id/produk/detail/ZENZATION-5W-30-API-SN");
+        initializeImageView(R.id.imageView22, "https://www.top1.co.id/produk/detail/ZENZATION-SUV-5W-30-API-SN");
+        initializeImageView(R.id.imageView24, "https://www.top1.co.id/produk/detail/ZENZATION-10W-40-API-SN");
+        initializeImageView(R.id.imageView26, "https://www.top1.co.id/produk/detail/ZENZATION-DIESEL-5W-30-API-CJ-4");
+        initializeImageView(R.id.imageView28, "https://www.top1.co.id/produk/detail/ZENZATION-DIESEL-10W-40-API-CJ-4");
+        initializeImageView(R.id.imageView32, "https://www.top1.co.id/produk/detail/HP-SPORT-10W-40-API-SN");
+        initializeImageView(R.id.imageView34, "https://www.top1.co.id/produk/detail/HP-SPORT-15W-40-API-SN");
+        initializeImageView(R.id.imageView02, "https://www.top1.co.id/produk/detail/ATF-LIFETIME-DIII");
+        initializeImageView(R.id.imageView04, "https://www.top1.co.id/produk/detail/ATF-EVOLUTION-MV");
+        initializeImageView(R.id.imageView06, "https://www.top1.co.id/produk/detail/ATF-CVT");
+        initializeImageView(R.id.imageView08, "https://www.top1.co.id/produk/detail/ATF-MV-PLUS");
+
+        // Inisialisasi inputTypeOli dan konfigurasi dialog pemilihan
         EditText inputTypeOli = findViewById(R.id.inputtypeoli);
         inputTypeOli.setFocusable(false);
         inputTypeOli.setClickable(true);
         inputTypeOli.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search, 0, R.drawable.segitiga_bawah, 0);
 
-        inputTypeOli.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                inputTypeOli.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search, 0, R.drawable.segitiga_atas, 0); // Ubah ke atas saat diklik
-                AlertDialog.Builder builder = new AlertDialog.Builder(spareparts.this);
-                builder.setTitle("Pilih Opsi");
+        inputTypeOli.setOnClickListener(view -> {
+            inputTypeOli.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search, 0, R.drawable.segitiga_atas, 0);
+            AlertDialog.Builder builder = new AlertDialog.Builder(spareparts.this);
+            builder.setTitle("Pilih Opsi");
 
-                final String[] options = {"EVOLUTION SERIES", "ZENZATION SERIES", "HP SPORT SERIES"};
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(spareparts.this, R.layout.item_with_ripple, options);
+            final String[] options = {"EVOLUTION SERIES", "ZENZATION SERIES", "HP SPORT SERIES"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(spareparts.this, R.layout.item_with_ripple, options);
 
-                builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        inputTypeOli.setHint(options[which]); // Mengubah hint berdasarkan pilihan yang dipilih
-                    }
-                });
+            builder.setAdapter(adapter, (dialog, which) -> {
+                inputTypeOli.setHint(options[which]);
+                setScrollViewVisibility(which);
+            });
 
-                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        inputTypeOli.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search, 0, R.drawable.segitiga_bawah, 0); // Kembali ke bawah setelah dismiss
-                    }
-                });
-
-                builder.show();
-            }
+            builder.setOnDismissListener(dialog -> inputTypeOli.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search, 0, R.drawable.segitiga_bawah, 0));
+            builder.show();
         });
 
+        // Inisialisasi tombol button1 dan button2 untuk mengatur visibilitas komponen
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
 
+        button1.setOnClickListener(v -> {
+            setVisibility(View.VISIBLE, View.GONE, View.GONE, View.GONE);
+        });
 
+        button2.setOnClickListener(v -> {
+            setVisibility(View.GONE, View.GONE, View.GONE, View.VISIBLE);
+        });
+    }
 
+    // Metode untuk menginisialisasi ImageView dengan URL
+    private void initializeImageView(int imageViewId, String url) {
+        ImageView imageView = findViewById(imageViewId);
+        imageView.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+    }
+
+    // Metode untuk mengatur visibilitas ScrollView berdasarkan pilihan
+    private void setScrollViewVisibility(int selectedIndex) {
+        findViewById(R.id.scroolviewevolution).setVisibility(selectedIndex == 0 ? View.VISIBLE : View.GONE);
+        findViewById(R.id.scroolviewzenzation).setVisibility(selectedIndex == 1 ? View.VISIBLE : View.GONE);
+        findViewById(R.id.scroolviewhpsport).setVisibility(selectedIndex == 2 ? View.VISIBLE : View.GONE);
+    }
+
+    // Metode untuk mengatur visibilitas komponen
+    private void setVisibility(int inputTypeOliVisibility, int evolutionVisibility, int zenzationVisibility, int oliTransmisiVisibility) {
+        findViewById(R.id.inputtypeoli).setVisibility(inputTypeOliVisibility);
+        findViewById(R.id.scroolviewevolution).setVisibility(evolutionVisibility);
+        findViewById(R.id.scroolviewzenzation).setVisibility(zenzationVisibility);
+        findViewById(R.id.olitransmisi).setVisibility(oliTransmisiVisibility);
     }
 }
